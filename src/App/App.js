a11c144beab0c4002d3b53d2bfb1469f1c184e76
Brendan Turner <kisbyturner@gmail.com>
2024-01-02 10:09:54 -0700

    Add reservation component and some styling to page

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,37 @@
 import './App.css';
 import React from 'react';
+import { useEffect, useState } from 'react';
+import Reservations from '../Reservations/Reservations';
 
 function App() {
+  const [reservations, setReservations] = useState([])
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/v1/reservations')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Server Error!');
+        }
+      })
+      .then((data) => {
+        setReservations(data);
+      })
+      .catch((error) => {
+        setError(error.message)
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
       <div className='resy-form'>
       </div>
       <div className='resy-container'>
+        <Reservations reservations={ reservations }/>
       </div>
     </div>
   );
---
+++ src/Reservations/Reservations.js
@@ -0,0 +1,24 @@
+import './Reservations.css';
+
+function Reservations({ reservations }) {
+    function showReservations() {
+        return reservations.map(reservation => {
+            return (
+                <section className='reservation-card'>
+                    <h1>{reservation.name}</h1>
+                    <p>{reservation.date}</p>
+                    <p>{reservation.time}</p>
+                    <p>Number of Guests: {reservation.number}</p>
+                    <button>Cancel</button>
+                </section>
+            )
+        })
+    }
+    return (
+        <div className='reservation-cards'>
+            {showReservations()}
+        </div>
+    )
+};
+
+export default Reservations;
